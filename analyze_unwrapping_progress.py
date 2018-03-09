426566a83a8e441a7f42f61f91089cd65253370c
kmaterna <kathrynmaterna@gmail.com>
2018-03-09 14:39:29 -0800

    color coding coh-baselines by summer/winter and duration

--- analyze_unwrapping_progress.py
+++ analyze_unwrapping_progress.py
@@ -37,6 +37,8 @@ def make_coh_vs_others_plots(stem1, stem2, mean_corr, stems_blt, xbaseline):
 		# Which season?
 		season.append(int(date1.strftime("%j")));
 
+	summer0=160;
+	summer1=300;
 
 	f, axarr = plt.subplots(3, figsize=(15, 15));
 	axarr[0].set_title('Mean Coherence of Sentinel-1A Interferograms',fontsize=24)
@@ -44,15 +46,25 @@ def make_coh_vs_others_plots(stem1, stem2, mean_corr, stems_blt, xbaseline):
 	axarr[0].set_xlabel('Time (days)',fontsize=20)
 	axarr[0].set_ylabel('Mean Coherence',fontsize=20)
 	axarr[0].tick_params(axis='both',labelsize=20)
-	axarr[1].plot(b_perp_baseline, mean_corr,'.',markersize=13);
+
+	for i in range(len(season)):
+		if season[i]>summer0 and season[i]<summer1:
+			line1, = axarr[1].plot(b_perp_baseline[i], mean_corr[i],'.',color='red',markersize=13, label='summer');
+		else:
+			line2, = axarr[1].plot(b_perp_baseline[i], mean_corr[i],'.',color='blue',markersize=13, label='not summer');
 	axarr[1].set_xlabel('Baseline (m)',fontsize=20)
 	axarr[1].set_ylabel('Mean Coherence',fontsize=20)
 	axarr[1].tick_params(axis='both',labelsize=20)
+	axarr[1].legend(handles=[line1, line2], loc=1, fontsize=18);
+
+
 	for i in range(len(season)):
 		if temporal_baseline[i]==12:
-			line1, =axarr[2].plot(season[i], mean_corr[i],'.',color='red',markersize=13, label='12 days');
+			line1, =axarr[2].plot(season[i], mean_corr[i],'.',color='magenta',markersize=13, label='12 days');
 		else:
 			line2, =axarr[2].plot(season[i], mean_corr[i],'.',color='green',markersize=13, label='>12 days');
+	axarr[2].plot([summer0, summer0],[0.05, 0.35],'--k');
+	axarr[2].plot([summer1, summer1],[0.05, 0.35],'--k');
 	axarr[2].set_xlabel('Day of Year',fontsize=20)
 	axarr[2].set_ylabel('Mean Coherence',fontsize=20)
 	axarr[2].legend(handles=[line1, line2],loc=2, fontsize=18)	
