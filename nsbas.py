21e1fc45670db86928a814281ac64a697c05b6fc
kmaterna <kathrynmaterna@gmail.com>
2018-03-16 17:16:32 -0700

    Begin setup of nsbas routine for pixel by pixel

--- nsbas.py
+++ nsbas.py
@@ -9,11 +9,19 @@ import glob, sys, math
 
 # ------------- CONFIGURE ------------ # 
 def configure():
-	file_dir="Sbas_tests/unwrap_ra";
+	file_dir="unwrap_ra";
+	
+	# Regular parameters
+	nsbas_num_toss=6;  # out of 62, how many interferograms can be incoherent? 
+	smoothing = 0;  # 
+	wavelength = 56;  # mm 
+
 	file_names=glob.glob(file_dir+"/*.grd");
 	if len(file_names)==0:
 		print("Error! No files matching search pattern."); sys.exit(1);
-	return file_names;
+	return [file_names, nsbas_num_toss, smoothing, wavelength];
+
+
 
 
 # ------------- INPUTS ------------ # 
@@ -23,7 +31,16 @@ def inputs(file_names):
 	for ifile in file_names:
 		data = read_grd(ifile);
 		data_all.append(data);
-	return [xdata, ydata, data_all];
+	date_pairs=[];
+	dates=[];
+	for name in file_names:
+		pairname=name.split('/')[-1][0:15];
+		date_pairs.append(pairname);  # returning something like '2016292_2016316' for each intf
+		splitname=pairname.split('_');
+		dates.append(splitname[0])
+		dates.append(splitname[1])
+	dates=list(set(dates));
+	return [xdata, ydata, data_all, dates, date_pairs];
 
 def read_grd(filename):
 	data0 = netcdf(filename,'r').variables['z'][::-1];
@@ -40,24 +57,68 @@ def read_grd_xy(filename):
 
 
 
+
+
 # ------------ COMPUTE ------------ #
-def compute(xdata, ydata, zdata_all):
+def compute(xdata, ydata, zdata_all, nsbas_num_toss, dates, date_pairs, smoothing, wavelength):
 	[zdim, xdim, ydim] = np.shape(zdata_all)
 	print(np.shape(zdata_all[::]))
-	analyze_coherent_number(zdata_all);
+	# number_of_datas = analyze_coherent_number(zdata_all);
+	analyze_velocity_nsbas(zdata_all, nsbas_num_toss, dates, date_pairs, smoothing, wavelength);
+	return ;
+
+
+def analyze_velocity_nsbas(zdata, nsbas_num_toss, dates, date_pairs, smoothing, wavelength):
+	# The point here is to loop through each pixel, determine if there's enough data to use, and then 
+	# make an SBAS matrix describing each image that's a real number (not nan). 
+	[zdim, xdim, ydim] = np.shape(zdata)
+	vel = np.zeros([xdim, ydim]);
+	
+	for i in range(xdim):  # A loop through each pixel. 
+		for j in range(ydim):
+			pixel_value = [zdata[k][i][j] for k in range(zdim)];  # slicing the values of phase for a pixel across the various interferograms
+			num_reals=0;
+			for k in range(zdim):
+				if not math.isnan(pixel_value[k]):
+					num_reals=num_reals+1;
+			
+			if num_reals > zdim - nsbas_num_toss:  # If we have a pixel that will be analyzed: Do SBAS
+
+				vel[i][j] = do_nsbas_pixel(pixel_value, dates, date_pairs, smoothing, wavelength); 
+				# pixel_value: if we have 62 intf, this is a (62,) array of the phase values in each interferogram. 
+				# dates: if we have 35 images, this is the date of each image
+				# date_pairs: if we have 62 intf, this is a (62) list with the image pairs used in each image
+				# This solves Gm = d for the movement of the pixel with smoothing. 
+
+				sys.exit(0);
+
+			else:
+				vel[i][j]=np.nan;
+				continue;
 
 	return;
 
 
 
-def analyze_coherent_number(zdata_all):
+def do_nsbas_pixel(pixel_value, dates, date_pairs, smoothing, wavelength):
+	# pixel_value: if we have 62 intf, this is a (62,) array of the phase values in each interferogram. 
+	# dates: if we have 35 images, this is the date of each image
+	# date_pairs: if we have 62 intf, this is a (62) list with the image pairs used in each image	
+	# for x in range(len(dates)-1):
+
+	return 0;
+
+
+
+
+def analyze_coherent_number(zdata):
 	# Analyze the number of coherent acquisitions for each pixel
-	[zdim, xdim, ydim] = np.shape(zdata_all)
+	[zdim, xdim, ydim] = np.shape(zdata)
 	number_of_datas=np.zeros([xdim,ydim]);
 	for k in range(zdim):
 		for i in range(xdim):
 			for j in range(ydim):
-				if not math.isnan(zdata_all[k][i][j]):
+				if not math.isnan(zdata[k][i][j]):
 					number_of_datas[i][j]=number_of_datas[i][j]+1;
 	plt.figure();
 	plt.imshow(number_of_datas);
@@ -69,17 +130,27 @@ def analyze_coherent_number(zdata_all):
 	plt.colorbar();
 	plt.savefig("number_of_coherent_intfs.eps");
 	plt.close();
-	return;
+	return number_of_datas;
+
+
+
+
+
 
 # ------------ COMPUTE ------------ #
 def outputs():
+	
 	return;
+
+
+
+
 
 
 
 
 if __name__=="__main__":
-	file_names = configure();
-	[xdata, ydata, data_all] = inputs(file_names);
-	compute(xdata, ydata, data_all);
+	[file_names, nsbas_num_toss, smoothing, wavelength] = configure();
+	[xdata, ydata, data_all, dates, date_pairs] = inputs(file_names);
+	compute(xdata, ydata, data_all, nsbas_num_toss, dates, date_pairs, smoothing, wavelength);
 	outputs();
