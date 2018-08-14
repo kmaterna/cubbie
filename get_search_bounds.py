4324d29be4d63c991a912dfc0a981f810a6e6e9c
kmaterna <kathrynmaterna@gmail.com>
2018-08-14 14:40:54 -0700

    changing print statements to python3

--- get_search_bounds.py
+++ get_search_bounds.py
@@ -3,7 +3,7 @@
 import sys
 
 if len(sys.argv) != 3:
-    print "Please provide the name of the search file and bounds file";
+    print("Please provide the name of the search file and bounds file");
     sys.exit(1);
 else:
     infile=open(sys.argv[1]);
@@ -15,13 +15,13 @@ point_index=temp.index('-p') if '-p' in temp else -1;
 region_index=temp.index('-r') if '-r' in temp else -1;
 
 if point_index != -1:
-    print "We are drawing a point."
+    print("We are drawing a point.")
     point_string=temp[point_index+1];
     temp2=point_string.split('/');
     outfile.write('%s %s\n' % ( temp2[0], temp2[1] ) );
     
 elif region_index != -1:
-    print "We are drawing a region."
+    print("We are drawing a region.")
     region_string=temp[region_index+1];
     temp2=region_string.split('/');
     outfile.write('%s %s\n' % ( temp2[0], temp2[2] ) );
@@ -30,7 +30,7 @@ elif region_index != -1:
     outfile.write('%s %s\n' % ( temp2[1], temp2[2] ) );
     outfile.write('%s %s\n' % ( temp2[0], temp2[2] ) );
 else:
-    print "could not find region or point."
+    print("could not find region or point.")
 
 infile.close();
 outfile.close();
