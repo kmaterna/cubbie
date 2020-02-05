03573343c5c561335fb3832c44dd87781180d203
Kathryn Materna <kathrynmaterna@gmail.com>
2020-02-04 16:47:17 -0800

    begin file to read data in isce format

---
+++ gdal_vrt_read_write.py
@@ -0,0 +1,56 @@
+# A set of functions that read and write vrt gdal grid files
+# Compatible with ISCE. 
+
+
+def read_complex_data(GDALfilename):
+    ds = gdal.Open(GDALfilename, gdal.GA_ReadOnly)
+    slc = ds.GetRasterBand(1).ReadAsArray()
+    transform = ds.GetGeoTransform()
+    ds = None
+    
+    # getting the min max of the axes
+    firstx = transform[0]
+    firsty = transform[3]
+    deltay = transform[5]
+    deltax = transform[1]
+    lastx = firstx+slc.shape[1]*deltax
+    lasty = firsty+slc.shape[0]*deltay
+    ymin = np.min([lasty,firsty])
+    ymax = np.max([lasty,firsty])
+    xmin = np.min([lastx,firstx])
+    xmax = np.max([lastx,firstx])
+
+    # put all zero values to nan and do not plot nan
+    try:
+        slc[slc==0]=np.nan
+    except:
+        pass
+
+    return slc;
+
+def read_scalar_data(GDALfilename):
+    ds = gdal.Open(GDALfilename, gdal.GA_ReadOnly)
+    data = ds.GetRasterBand(band).ReadAsArray()
+    transform = ds.GetGeoTransform()
+    ds = None
+    
+    # getting the min max of the axes
+    firstx = transform[0]
+    firsty = transform[3]
+    deltay = transform[5]
+    deltax = transform[1]
+    lastx = firstx+data.shape[1]*deltax
+    lasty = firsty+data.shape[0]*deltay
+    ymin = np.min([lasty,firsty])
+    ymax = np.max([lasty,firsty])
+    xmin = np.min([lastx,firstx])
+    xmax = np.max([lastx,firstx])
+
+    # put all zero values to nan and do not plot nan
+    if background is None:
+        try:
+            data[data==0]=np.nan
+        except:
+            pass
+
+    return data;
