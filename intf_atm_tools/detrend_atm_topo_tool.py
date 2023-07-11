ac04536a4a2594f442014c6176be5657e4966b1c
kmaterna <kathrynmaterna@gmail.com>
2023-07-11 08:53:08 -0700

    refactoring for new remove-xy-plane option and separating plot functions

--- intf_atm_tools/detrend_atm_topo_tool.py
+++ intf_atm_tools/detrend_atm_topo_tool.py
@@ -7,10 +7,9 @@ Remove the topography-correlated trend and save the adjusted image to a file.
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
-import argparse
+import argparse, scipy
 from Tectonic_Utils.read_write import netcdf_read_write as rw
-
+from S1_batches.intf_atm_tools import plots
 
 def arg_parser():
     p = argparse.ArgumentParser(description=__doc__)
@@ -20,6 +19,7 @@ def arg_parser():
                    required=True);
     p.add_argument('-r', '--detrend_topography', type=bool, default=True,
                    help='''Remove topography-correlated trend, default is True''');
+    p.add_argument('-w', '--produce_plots', type=bool, default=True, help='''Plot the before-and-after images''');
     p.add_argument('-m', '--mask_polygon', type=str, help='''Future: will have a masked polygon feature''');
     p.add_argument('-p', '--remove_xy_plane', type=bool, default=False,
                    help='''Future: will have planar removal feature, default is False''');
@@ -31,32 +31,27 @@ def arg_parser():
 
 def coordinator(exp_dict):
     """ Driver for the whole command-line program. """
-    xdata, ydata, phase_data, demdata = data_reader(exp_dict);
+    [xdata, ydata, phase_data] = rw.read_any_grd(exp_dict['data_file']);
     corrected_phase_2d = phase_data.copy();
     if exp_dict['detrend_topography']:
-        [phase_1d, dem_1d, corrected_phase_1d, corrected_phase_2d] = correct_for_topo_trend(corrected_phase_2d, demdata)
-        output_plots(phase_1d, dem_1d, corrected_phase_1d, phase_data, corrected_phase_2d, exp_dict['outname']);
+        [_, _, demdata] = rw.read_any_grd(exp_dict['dem_file']);
+        corrected_phase_2d = correct_for_topo_trend(corrected_phase_2d, demdata, exp_dict);
     if exp_dict['remove_xy_plane']:
-        corrected_phase_2d = correct_for_plane(xdata, ydata, corrected_phase_2d);
+        corrected_phase_2d = correct_for_plane(xdata, ydata, corrected_phase_2d, exp_dict);
     rw.produce_output_netcdf(xdata, ydata, corrected_phase_2d, 'unwrapped_phase', exp_dict['outname']);
     return;
 
 
-def data_reader(exp_dict):
-    [_, _, demdata] = rw.read_any_grd(exp_dict['dem_file']);
-    [xdata, ydata, phase_data] = rw.read_any_grd(exp_dict['data_file']);
-    return xdata, ydata, phase_data, demdata;
-
-
 # ---------- COMPUTE FUNCTIONS ------------ #
-def correct_for_topo_trend(phasedata, demdata):
+def correct_for_topo_trend(phasedata, demdata, exp_dict):
     """
     :param phasedata: 2d array of unwrapped phase values
     :param demdata: 2d array of topography, same size as zdata
+    :param exp_dict: dictionary of parameters, including outfilename
     :returns: 2d array of corrected unwrapped phase values
     """
     print("Removing topography-correlated trend.")
-    defensive_checks(phasedata, demdata);
+    defensive_checks_topo_phase(phasedata, demdata);
     full_phase_array_1d, full_dem_array_1d = np.ravel(phasedata), np.ravel(demdata);
     phase_array_1d = full_phase_array_1d[np.where(~np.isnan(full_phase_array_1d))];  # non-nan data to 1D arrays
     demarray_1d = full_dem_array_1d[np.where(~np.isnan(full_phase_array_1d))];
@@ -64,22 +59,37 @@ def correct_for_topo_trend(phasedata, demdata):
     corrected_array_1d = full_phase_array_1d - coef[0] * full_dem_array_1d;  # Remove slope
     corrected_array_2d = np.reshape(corrected_array_1d, np.shape(phasedata));  # Reshape back into right shape
     print("Best-fitting Slope: %f " % coef[0]);
-    return [full_phase_array_1d, full_dem_array_1d, corrected_array_1d, corrected_array_2d];
+    if exp_dict["produce_plots"]:
+        plots.before_after_images(phasedata, corrected_array_2d,
+                                  outfilename=exp_dict['outname'].split('.grd')[0] + '_before_after.png');
+        plots.linear_topo_phase_plot(phase_array_1d, demarray_1d, corrected_array_1d,
+                                     outfilename=exp_dict['outname'].split('.grd')[0] + '_phase_topo.png');
+    return corrected_array_2d;
 
 
-def correct_for_plane(_xdata, _ydata, phasedata):
+def correct_for_plane(xdata, ydata, phasedata, exp_dict):
     """
-    :param _xdata: 1d array
-    :param _ydata: 1d array
+    :param xdata: 1d array
+    :param ydata: 1d array
     :param phasedata: 2d array
+    :param exp_dict: dictionary of parameter values
     :returns: 2d array of corrected unwrapped phase values
     """
     print("Removing bilinear plane.")
-    corrected_phasedata = phasedata.copy();
-    return corrected_phasedata;
+    X, Y = np.meshgrid(xdata, ydata);
+    full_x, full_y, full_phase = np.ravel(X), np.ravel(Y), np.ravel(phasedata);
+    x_nonans = full_x[np.where(~np.isnan(full_phase))];
+    y_nonans = full_y[np.where(~np.isnan(full_phase))];
+    phase_nonans = full_phase[np.where(~np.isnan(full_phase))];
+    coeffs = scipy.linalg.lstsq();  # the actual optimization step
+    corrected_phase_2d = phasedata.copy();
+    if exp_dict["produce_plots"]:
+        plots.before_after_images(phasedata, corrected_phase_2d,
+                                  outfilename=exp_dict['outname'].split('.grd')[0] + '_before_after_planar.png');
+    return corrected_phase_2d;
 
 
-def defensive_checks(zdata, demdata):
+def defensive_checks_topo_phase(zdata, demdata):
     """
     :param zdata: 2d array of unwrapped phase
     :param demdata: 2d array of dem information
@@ -92,37 +102,6 @@ def defensive_checks(zdata, demdata):
     return;
 
 
-# --------------- OUTPUTS ---------------- #
-def output_plots(phase_array_1d, dem_array_1d, corrected_array_1d, phase_2d, corrected_array_2d, outgrdname):
-    plt.figure();
-    plt.plot(dem_array_1d, phase_array_1d, '.', label='Before correction');
-    plt.plot(dem_array_1d, corrected_array_1d, '.r', alpha=0.15, label='After correction')
-    plt.ylabel('phase');
-    plt.xlabel('topo');
-    plt.title('Initial and Corrected Phase vs. Topography');
-    plt.legend();
-    outfilename = outgrdname.split('.grd')[0] + '_phase_topo.png'
-    print("Saving figure %s " % outfilename);
-    plt.savefig(outfilename);
-    plt.close();
-
-    plt.figure();
-    plt.subplot(1, 2, 1);
-    plt.imshow(phase_2d, cmap='hsv', vmin=np.min(phase_array_1d), vmax=np.max(phase_array_1d));
-    plt.title('Before Correction');
-    plt.subplot(1, 2, 2);
-    plt.imshow(corrected_array_2d, cmap='hsv', vmin=np.min(phase_array_1d), vmax=np.max(phase_array_1d));
-    plt.title('After Correction');
-    cb = plt.colorbar();
-    cb.set_label("Unwrapped Phase (radians)", size=12);
-    outfilename = outgrdname.split('.grd')[0] + '_before_after.png'
-    print("Saving figure %s " % outfilename);
-    plt.savefig(outfilename);
-    plt.close();
-    return;
-
-
 if __name__ == "__main__":
-    """Example runstring: detrend_atm_topo_tool.py detrend_atm_topo_tool.py unwrap.grd dem.grd detrended_unwrap.grd"""
     exp_dict = arg_parser();
     coordinator(exp_dict);
---
+++ intf_atm_tools/plots.py
@@ -0,0 +1,38 @@
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def before_after_images(before_phase, after_phase, outfilename):
+    """
+    Produce a comparison plot of before-and-after images of unwrapped phase.
+    """
+    plt.figure(figsize=(12, 7), dpi=300);
+    plt.subplot(1, 2, 1);
+    plt.imshow(before_phase, cmap='viridis', vmin=np.min(before_phase), vmax=np.max(after_phase));
+    plt.title('Before Correction');
+    plt.subplot(1, 2, 2);
+    plt.imshow(after_phase, cmap='viridis', vmin=np.min(before_phase), vmax=np.max(after_phase));
+    plt.title('After Correction');
+    cb = plt.colorbar();
+    cb.set_label("Unwrapped Phase (radians)", size=12);
+    print("Saving figure %s " % outfilename);
+    plt.savefig(outfilename);
+    plt.close();
+
+def linear_topo_phase_plot(phase_array_1d, dem_array_1d, corrected_phase_1d, outfilename):
+    """
+    Plot topography and phase as x-y scatter plot.
+    """
+    plt.figure(figsize=(8, 6), dpi=300);
+    plt.plot(dem_array_1d, phase_array_1d, '.', label='Before correction');
+    plt.plot(dem_array_1d, corrected_phase_1d, '.r', alpha=0.15, label='After correction')
+    plt.ylabel('phase');
+    plt.xlabel('topo');
+    plt.title('Initial and Corrected Phase vs. Topography');
+    plt.legend();
+    print("Saving figure %s " % outfilename);
+    plt.savefig(outfilename);
+    plt.close();
+    return;
