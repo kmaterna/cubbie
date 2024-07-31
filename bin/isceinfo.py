a88672839ec091d4530bfc61c030fd74c96068ce
Kathryn Materna <kathrynmaterna@gmail.com>
2024-07-31 14:44:59 -0600

    developing isceinfo.py for complex and two-band files

--- bin/isceinfo.py
+++ bin/isceinfo.py
@@ -5,6 +5,7 @@ The equivalent of gmt grdinfo for isce files, including displaying the number of
 
 import numpy as np
 from s1_batches.read_write_insar_utilities import isce_read_write
+from s1_batches.math_tools import phase_math
 import matplotlib.pyplot as plt
 import argparse
 
@@ -24,18 +25,34 @@ def parse_arguments():
                    help='''Maximum color for quick-and-dirty plot.''', default=None)
     p.add_argument('-p', '--plot_data',
                    help='''Whether to plot data or not''', default=True)
+    p.add_argument('-b', '--band', type=int,
+                   help='''Band of data''', default=1)
+    p.add_argument('--take_phase', action="store_true",
+                   help='''Convert complex data into phase''')
+    p.add_argument('--take_amplitude', action="store_true",
+                   help='''Convert complex data into amplitude''')
     exp_dict = vars(p.parse_args())
     return exp_dict
+
+
+def optional_complex_preprocess(data, args):
+    new_data = data
+    if isinstance(data[0][0], np.complex64) and args['take_phase']:
+        new_data, _ = phase_math.real_imag2phase_amp(np.real(data), np.imag(data))
+    if isinstance(data[0][0], np.complex64) and args['take_amplitude']:
+        _, new_data = phase_math.real_imag2phase_amp(np.real(data), np.imag(data))
+    return new_data
 
 
 def main_body(args):
     sourcefile = args['data_file']
 
     if '.unw' in sourcefile:
-        xdata, ydata, data = isce_read_write.read_scalar_data(sourcefile)  # reading second band
+        xdata, ydata, data = isce_read_write.read_scalar_data(sourcefile, band=args['band'])  # reading second band
     else:
-        xdata, ydata, data = isce_read_write.read_scalar_data(sourcefile)
+        xdata, ydata, data = isce_read_write.read_scalar_data(sourcefile, band=args['band'])
 
+    print("Reading band %s" % str(args['band']))
     print("Range of X axis: xmin:", np.nanmin(xdata), "xmax:", np.nanmax(xdata),
           ", xlen:", len(xdata), ", xinc:", xdata[1]-xdata[0])
     print("Range of Y axis: ymin:", np.nanmin(ydata), "ymax:", np.nanmax(ydata),
@@ -44,6 +61,9 @@ def main_body(args):
     print("Shape of data: ", np.shape(data))
     num_values = np.size(data)
     print("Number of values: ", num_values)
+
+    data = optional_complex_preprocess(data, args)
+
     print("Spread of data: zmin:", np.nanmin(data), ", zmax:", np.nanmax(data))
     num_nans = np.sum(np.isnan(data))
     percent_nans = (num_nans / num_values) * 100
@@ -52,9 +72,9 @@ def main_body(args):
     if args['plot_data']:
         plt.figure()
         vmin = args['vmin'] if args['vmin'] is not None else np.nanmin(data)
-        vmax = args['vmax'] if args['vmax'] is not None else np.nanmin(data)
-        plt.imshow(data, vmin=vmin, vmax=vmax)
-        plt.colorbar()
+        vmax = args['vmax'] if args['vmax'] is not None else np.nanmax(data)
+        ms = plt.imshow(data, vmin=vmin, vmax=vmax)
+        plt.colorbar(ms)
         plt.show()
     return
 
--- math_tools/phase_math.py
+++ math_tools/phase_math.py
@@ -1,50 +1,31 @@
 import numpy as np
-import math
 import sys
 
 
 def real_imag2phase_amp(real, imag):
     """
-    Simple math function operating on 1-d arrays.
+    Simple math function operating on arrays.
 
-    :param real: 1-d array
-    :param imag: 1-d array
-    :returns: [1-d array phase, 1-d array amp]
+    :param real: np.array
+    :param imag: np.array
+    :returns: [np.array phase, np.array amp]
     """
-    phase, amp = [], []
-    count = 0
-    for x in range(len(imag)):
-        phase.append(np.arctan2(imag[x], real[x]))
-        amp.append(np.sqrt(real[x] * real[x] + imag[x] * imag[x]))
-        if math.isnan(np.arctan2(imag[x], real[x])):
-            count = count + 1
-    if count > 0:
-        print("Warning: %d of %d are nans" % (count, len(real)))
-    else:
-        print("Converted real,imag to phase,amp with no nans.")
-    return [phase, amp]
+    phase = np.arctan2(imag, real)
+    amp = np.sqrt(np.square(real) + np.square(imag))
+    return phase, amp
 
 
 def phase_amp2real_imag(phase, amp):
     """
-    Simple math function operating on 1-d arrays.
+    Simple math function operating on numpy arrays.
 
-    :param phase: 1-d array
-    :param amp: 1-d array
-    :returns: [1-d array real, 1-d array imag]
+    :param phase: np.array
+    :param amp: np.array
+    :returns: np.array real, np.array imag
     """
-    real, imag = [], []
-    count = 0
-    for i in range(len(phase)):
-        real.append(amp[i] * np.cos(phase[i]))
-        imag.append(amp[i] * np.sin(phase[i]))
-        if math.isnan(amp[i] * np.sin(phase[i])):
-            count = count + 1
-    if count > 0:
-        print("Warning: %d of %d are nans" % (count, len(real)))
-    else:
-        print("Converted phase,amp to real,imag with no nans.")
-    return [real, imag]
+    real = amp * np.cos(phase)
+    imag = amp * np.sin(phase)
+    return real, imag
 
 
 def develop_mean_phase(phase_array):
